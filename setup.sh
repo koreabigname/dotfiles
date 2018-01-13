77724b346b611160e3c9d8110e143e1901f64947
Jingun Jung <jingun.jung@motojin.com>
2018-01-13 17:28:53 +0900

    Add vimrc file

--- setup.sh
+++ setup.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-DOT_FILES=(zshrc)
+DOT_FILES=(vimrc zshrc)
 
 # Make link
 for dot_file in ${DOT_FILES[@]}
 do
   ln -sf ~/.dotfiles/${dot_file} $HOME/.${dot_file}
 done
+
+# Install dein and oh-my-zsh
+[ ! -d ~/.vim/bundle ] && curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > ~/installer.sh && sh ~/installer.sh ~/.vim/bundle && rm ~/installer.sh
+[ ! -d ~/.oh-my-zsh ] && git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
 exit 0
